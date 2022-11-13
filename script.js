b0b67f88e1691eb38c386a5f1d2248ee7a12f65b
angelique KRAUSE <krause_angelique@hotmail.com>
2022-11-13 15:33:07 +0100

    structure TREE

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('bonjour');
+
+/* Préparation du programme */
+
+// premier item sélectionné
+    //  input:checked+p 
+
+// dernier item sélectionné
+    // input:checked+p
+    // shift
+/*
+L'utilisateur clique avec la souris sur un input. 
+L'utilisateur appuie sur une touche (shift) du clavier.
+=> tous les items div.item input:checked+p soit SOULIGNÉ
+*/
+
+
+
+ // https://developer.mozilla.org/fr/docs/Learn/JavaScript/First_steps   
